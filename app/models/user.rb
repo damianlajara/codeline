1a857c2be1eb7e31c364e1f295de84a95424deae
Damian Lajara <damiansito93@gmail.com>
2015-11-14 14:02:37 -0500

    add friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
   validates_presence_of :username
   validates_uniqueness_of :username
+
+  # user.friendships will only return the friendships the user requested
+  has_many :friendships, dependent: :destroy
+  # user.inverse_friendships will return the friendships accepted by the other user
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+
+  def request_friendship(friend)
+    self.friendships.create(friend: friend)
+  end
 end
