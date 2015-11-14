1742023ecedbe64842d6ae24033cc2b1eb659cfc
Damian Lajara <damiansito93@gmail.com>
2015-11-14 13:37:00 -0500

    add user index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   # The priority is based upon order of creation: firsst created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
