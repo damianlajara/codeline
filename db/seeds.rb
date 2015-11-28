14e2b2713af5998407d3d332e4461a3845047b71
Damian Lajara <damiansito93@gmail.com>
2015-11-27 22:57:38 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,3 +38,15 @@ User.create([
   }
 ])
 p "Test users created"
+
+Friendship.create([
+  {
+    user_id: 1,
+    friend_id: 1
+  },
+  {
+    user_id: 2,
+    friend_id: 1
+  },
+])
+p "Friendships created"
